c6f8d85a0e6daefc924e05068a0d798296296086
Michelle <angelamaincs@gmail.com>
2022-11-24 08:39:14 -0800

    snake game, day 21, add the class inheritance and list slice function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,6 +11,8 @@ screen.title("Snake Game 2022")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
@@ -21,7 +25,24 @@ should_game = True
 while should_game:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_scoreboard()
+
+    # detect collision with wall.
+    if snake.head.xcor() > 200 or snake.head.xcor() <-200\
+            or snake.head.ycor() > 200 or snake.head.ycor()<-200:
+        should_game = False
+        scoreboard.game_over()
+
+    # detect collision with tail.
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            should_game = False
+            scoreboard.game_over()
 
 screen.exitonclick()
