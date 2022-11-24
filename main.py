9c0516ff57bffa54a1608177c5e3fd5a015838bc
Michelle <angelamaincs@gmail.com>
2022-11-23 22:44:00 -0800

    snake game

---
+++ main.py
@@ -0,0 +1,27 @@
+from turtle import Screen, Turtle
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width=600, height = 600)
+screen.bgcolor("black")
+screen.title("Snake Game 2022")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+
+should_game = True
+
+while should_game:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+screen.exitonclick()
